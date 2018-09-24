a20963a8eed32a3acab7c8ee87ecdfc21c016421
Johan <johan@anyfin.com>
2018-09-24 19:32:47 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print('hello world')
+import gym
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())  # take a random action
